9d93b2e1c8ad5381e4acc26f14f7f3e5ed9a983f
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2025-04-16 19:39:01 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74/
+
+function quote(fighter) {
+  if (fighter.toLowerCase() === "george saint pierre") {
+    return "I am not impressed by your performance.";
+  }
+  if (fighter.toLowerCase() === "conor mcgregor") {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+  }
+}
