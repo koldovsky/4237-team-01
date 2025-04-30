4f5c386331e9cbe7b3090b4ebbd0224f02addd3d
Semen Levchenko <117591581+Currahae@users.noreply.github.com>
2025-04-30 00:35:21 +0000

    Add base version of beer carousel

--- js/index.beer-carousel.js
+++ js/index.beer-carousel.js
@@ -3,7 +3,7 @@ const beerCardsContent = [
         id: 1,
         image: "image/beer-carousel/lvivske-beer-bottle.webp",
         name: "Lvivske Lager",
-        recommendation: "Ideal for soulful chill with friends.",
+        recommendation: "Ideal for soulful chill with friends and watch sunsets at 4 a.m..",
         abv: "ABV - 4.5%",
         ibu: "IBU - 20",
         flavour: "The vibe, taste of youth, and student life."
@@ -21,26 +21,26 @@ const beerCardsContent = [
         id: 2,
         image: "image/beer-carousel/lager-beer.webp",
         name: "Lager",
-        recommendation: "Perfect for those who enjoy light and refreshing beer.",
+        recommendation: "This classic bottled beer features a wheat aroma and mild taste.",
         abv: "ABV - 5%",
         ibu: "IBU - 20",
         flavour: "Crisp, clean, with a hint of malt sweetness."
     },
     {
         id: 3,
-        image: "image/beer-carousel/stout-beer.webp",
-        name: "Stout",
-        recommendation: "Great for fans of dark and rich beer with roasted flavors.",
-        abv: "ABV - 6%",
-        ibu: "IBU - 50",
-        flavour: "Rich, creamy, with notes of coffee and chocolate."
+        image: "image/beer-carousel/bitter-beer.webp",
+        name: "BITTER",
+        recommendation: "'England's favorite beer' is a great choice for those who like dark varieties of beer.",
+        abv: "ABV - 4.6%",
+        ibu: "IBU - 40",
+        flavour: "Bitter, malty, with a slight aftertaste of toasts."
     }
 ];
 
 function renderBeerCards(content) {
-    let beerCardsHTML = "";
+    let beerCardsHTML = [];
     for (let card of content) {
-        beerCardsHTML += `
+        beerCardsHTML.push(`
             <div class="beer-carousel__card">
                 <div class="beer-carousel__card-img-wrap">
                     <img height="300" class="beer-carousel__card-img" src="${card.image}" alt="${card.name} beer bottle">
@@ -52,10 +52,38 @@ function renderBeerCards(content) {
                     <p class="beer-carousel__card-ibu">${card.ibu}</p>
                     <p class="beer-carousel__card-flavour">${card.flavour}</p>
                 </div>
-            </div>`;
+            </div>`);
     }
-    const beerCarouselBelt = document.querySelector(".beer-carousel__belt");
-    beerCarouselBelt.innerHTML = beerCardsHTML;
+    return beerCardsHTML;
+    // const beerCarouselBelt = document.querySelector(".beer-carousel__belt");
+    // beerCarouselBelt.innerHTML = beerCardsHTML;
 }
 
-renderBeerCards(beerCardsContent);
+const beerCardsSlides = renderBeerCards(beerCardsContent);
+let currentSlide = 0;
+const totalSlides = beerCardsSlides.length;
+
+function renderCarousel() {
+    const beerCarouselBelt = document.querySelector(".beer-carousel__belt");
+    beerCarouselBelt.innerHTML = beerCardsSlides[currentSlide];
+    const secondSlide = (currentSlide + 1) % totalSlides;
+    beerCarouselBelt.innerHTML += beerCardsSlides[secondSlide];
+
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    renderCarousel();
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    renderCarousel();
+}
+
+const nextButton = document.querySelector(".beer-carousel__button--next");
+const prevButton = document.querySelector(".beer-carousel__button--prev");
+nextButton.addEventListener("click", nextSlide);
+prevButton.addEventListener("click", prevSlide);
+
+renderCarousel();
