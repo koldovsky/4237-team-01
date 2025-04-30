9044d7b4ef35c90786611ca84198293314006838
AlonaZahychenko <zahychenko.alona8@gmail.com>
2025-04-30 11:22:21 +0000

    add map-init.js style map

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+import("./map-init.js");
+
 function init() {
     import("./global.header-burger.js");
     import("./index.beer-carousel.js");
+
 }
 
 const totalPartials = document.querySelectorAll(
