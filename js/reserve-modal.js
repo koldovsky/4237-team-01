dd0fbc4028febd1bf5bf35ad760f179df2d3c340
AlonaZahychenko <zahychenko.alona8@gmail.com>
2025-05-03 16:44:53 +0000

    continue reserve-modal.js fix

--- js/reserve-modal.js
+++ js/reserve-modal.js
@@ -1,36 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
+function initReserveModal() {
+
     const form = document.querySelector('.contacts__form-wrapper');
     const modal = document.querySelector('.reservation-modal');
     const closeBtn = document.querySelector('[data-role="close"]');
-    const okBtn = document.querySelector('reservation-modal__button');
-})
+    const okBtn = document.querySelector('.reservation-modal__button');
 
-if (form && modal) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    if (form && modal) {
+        console.log('form and modal found');
 
-        const inputs = form.querySelectorAll('.contacts__input');
-        let isValid = true;
 
-        inputs.forEach(input => {
-            input.classList.remove('input-error');
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-            if (!input.value.trim()) {
-                isValid = false;
-                input.classList.add('input-error');
+            const inputs = form.querySelectorAll('.contacts__input');
+            let isValid = true;
+
+            inputs.forEach(input => {
+                input.classList.remove('input-error');
+
+                if (!input.value.trim()) {
+                    isValid = false;
+                    input.classList.add('input-error');
+                }
+            });
+
+            if (isValid) {
+                modal.classList.add('is-active');
             }
         });
 
-        if (isValid) {
-            modal.classList.add('is-active');
-        }
-    });
+        closeBtn?.addEventListener('click', () => {
+            modal.classList.remove('is-active');
+        });
 
-    closeBtn.addEventListener('click', () => {
-        modal.classList.remove('is-active');
-    });
-
-    okBtn.addEventListener('click', () => {
-        modal.classList.remove('is-active');
-    });
+        okBtn?.addEventListener('click', () => {
+            modal.classList.remove('is-active');
+        });
+    }
 }
+
+document.body.addEventListener('htmx:afterSwap', (e) => {
+    if (e.detail.target.querySelector('.contacts__form-wrapper') ||
+    e.detail.target.querySelector('.reservation-modal')) {
+        initReserveModal();
+    }
+});
+
