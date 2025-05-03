cf884ba2165c94bb96b0f9555211231313f78152
AlonaZahychenko <zahychenko.alona8@gmail.com>
2025-05-03 23:14:23 +0000

    fix reserve-module

--- js/index.js
+++ js/index.js
@@ -1,11 +1,24 @@
+import reserveModal from "./reserve-modal.js";
+
 function init() {
     import("./global.header-burger.js");
     import("./index.beer-carousel.js");
     import("./index.latest-posts.js").then((module) => module.default());
     import("./index.friday-deal.js");
-    import("./map-init.js");
-    import("./reserve-modal.js");
+    //import("./reserve-modal.js");
 }
+
+document.body.addEventListener('htmx:afterSwap', (e) => {
+    console.log('htmx:afterSwap triggered');
+    const form = document.querySelector('.contacts__form-wrapper');
+    const modal = document.querySelector('.reservation-modal');
+    console.log('Form found:', form);
+    console.log('Modal found:', modal);
+
+    if (form || modal) {
+        reserveModal();
+    }
+});
 
 const totalPartials = document.querySelectorAll(
     '[hx-trigger="load"], [data-hx-trigger="load"]'
--- js/reserve-modal.js
+++ js/reserve-modal.js
@@ -1,5 +1,6 @@
-function initReserveModal() {
+console.log('reserve-modal.js loaded');
 
+export default function reserveModal() {
     const form = document.querySelector('.contacts__form-wrapper');
     const modal = document.querySelector('.reservation-modal');
     const closeBtn = document.querySelector('[data-role="close"]');
@@ -8,9 +9,9 @@ function initReserveModal() {
     if (form && modal) {
         console.log('form and modal found');
 
-
         form.addEventListener('submit', (e) => {
             e.preventDefault();
+            console.log('form submitted');
 
             const inputs = form.querySelectorAll('.contacts__input');
             let isValid = true;
@@ -25,6 +26,7 @@ function initReserveModal() {
             });
 
             if (isValid) {
+                console.log('Form is valid, showing modal');
                 modal.classList.add('is-active');
             }
         });
@@ -36,13 +38,10 @@ function initReserveModal() {
         okBtn?.addEventListener('click', () => {
             modal.classList.remove('is-active');
         });
+    } else {
+        console.log('form or modal not found');
     }
 }
 
-document.body.addEventListener('htmx:afterSwap', (e) => {
-    if (e.detail.target.querySelector('.contacts__form-wrapper') ||
-    e.detail.target.querySelector('.reservation-modal')) {
-        initReserveModal();
-    }
-});
+
 
